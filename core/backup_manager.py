90406d4ac6683a7d7d07894aaa1c3d9b92d3df7a
John Nigdelis <gnigdelis@sunsoft.gr>
2026-07-20 12:19:28 +0300

    feat(core): implement backup manager architecture

--- core/backup_manager.py
+++ core/backup_manager.py
@@ -1,34 +1,50 @@
-class SunsoftBackupAgent:
+class BackupManager:
 
-    def initialize(self):
+    def start_backup(self):
+
         pass
+
 
     def validate_environment(self):
+
         pass
+
 
     def backup_files(self):
+
         pass
+
 
     def backup_registry(self):
+
         pass
+
 
     def backup_sql(self):
+
         pass
+
 
     def compress_backup(self):
+
         pass
+
 
     def upload_backup(self):
+
         pass
+
 
     def verify_backup(self):
+
         pass
+
 
     def cleanup(self):
+
         pass
 
-    def update_ui(self):
-        pass
 
-    def shutdown(self):
+    def finish_backup(self):
+
         pass
